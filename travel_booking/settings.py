94870b7fe396c3173a771254cfe3b462b9bbde59
Gaurang Gupta <guptagaurang0112@gmail.com>
2025-08-29 18:02:11 +0530

    changes

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -72,6 +72,8 @@ WSGI_APPLICATION = 'travel_booking.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+import os # Add this if it's not already there
 
 DATABASES = {
     'default': {
@@ -79,6 +81,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Production database configuration using Render's DATABASE_URL
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True # Render databases require SSL
+    )
 
 
 # Password validation
