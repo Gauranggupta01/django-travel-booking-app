1f90314c65dce972e7bd38f3429828fadb7e9c6e
Gaurang Gupta <guptagaurang0112@gmail.com>
2025-08-29 18:13:50 +0530

    changes

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -156,3 +156,5 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://django-travel-booking-app.onrender.com']
